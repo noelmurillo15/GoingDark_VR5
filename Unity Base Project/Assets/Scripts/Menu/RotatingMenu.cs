d8dc41ca087f5159d129294e43da516fc4ef5b8f
Allan Murillo <noelmurillo15@fullsail.edu>
2016-04-18 14:39:41 -0400

    Second Commit

--- Unity Base Project/Assets/Scripts/Menu/RotatingMenu.cs
+++ Unity Base Project/Assets/Scripts/Menu/RotatingMenu.cs
@@ -1,20 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class RotatingMenu : MonoBehaviour {
+public class RotatingMenu : MonoBehaviour
+{
 
     private float velocity;
     private LeapData m_leapData;
 
 
     // Use this for initialization
-    void Start() { 
+    void Start()
+    {
         if (m_leapData == null)
             m_leapData = GameObject.FindGameObjectWithTag("LeapControl").GetComponent<LeapData>();
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         if (velocity > 0.2f)
             velocity -= 20 * Time.deltaTime;
         else if (velocity < -0.2f)
@@ -22,7 +25,7 @@ public class RotatingMenu : MonoBehaviour {
         else
             velocity = 0.0f;
 
-    
+
         if (m_leapData.GetLPalmVelocity().x > 500.0f && m_leapData.GetLPalmNormals().x > 0.0f)
             velocity = -m_leapData.GetLPalmVelocity().x * Time.deltaTime * 0.50f;
         else if (m_leapData.GetLPalmVelocity().x < -500.0f && m_leapData.GetLPalmNormals().x < 0.0f)
