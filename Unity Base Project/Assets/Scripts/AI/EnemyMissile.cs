da730c2201360c15d30159f9fd7d386e6052eb14
Allan Murillo <noelmurillo15@fullsail.edu>
2016-04-20 13:49:49 -0400

    enemy locks on and fires seeking missile

---
+++ Unity Base Project/Assets/Scripts/AI/EnemyMissile.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMissile : MonoBehaviour {
+
+    public float Speed;
+    public int LookSpeed;
+    public float TimeTillTrack;
+    public float Timer;
+    public float DistanceTillStopLooking;
+    public float CalculatedDistance;
+    public Quaternion targetRotation;
+    public GameObject Player;
+    public GameObject Explosion;
+    public bool stopTurning;
+    public int TimeTillExpire;
+    public bool Die;
+    public bool startTracking;
+
+    private Vector3 Target;
+
+    public float DestroyTimer;
+
+    //void Awake()
+    //{
+    //    startTracking = true;
+    //}
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+
+        if (Player != null)
+            Target = Player.transform.position;
+
+        Die = false;
+
+        Speed = 50.0f;
+        DestroyTimer = 10.0f;
+        LookSpeed = 2;
+    }
+
+    void Update()
+    {
+
+        if (Player != null)
+            Target = Player.transform.localPosition;
+
+        if (DestroyTimer > 0.0f)
+            DestroyTimer -= Time.deltaTime;
+
+        CalculatedDistance = Vector3.Distance(gameObject.transform.position, Target);
+
+        if (!Die)
+            transform.Translate(0, 0, Speed * Time.deltaTime);
+
+        if (!Die && startTracking)
+        {
+            targetRotation = Quaternion.LookRotation(Target - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * LookSpeed);
+        }
+
+        if (DestroyTimer <= 0.0f)
+            Destroy(this.gameObject, 0);
+
+        if (Die)
+        {
+            if (Explosion != null)
+            {
+                Instantiate(Explosion, transform.position, transform.rotation);
+                Explosion = null;
+                this.GetComponent<MeshRenderer>().enabled = false;
+            }
+
+            if (DestroyTimer <= 0.0f)
+                Destroy(this.gameObject, 0);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Collision with : " + col.gameObject.name);
+       
+        if (col.gameObject.tag == "Player")
+        {
+           Die = true;
+           Debug.Log("Missile Hit Player");
+           DestroyTimer = 2.5f;
+           col.gameObject.SendMessage("Hit");
+        }
+    }
+}
--- Unity Base Project/Assets/Scripts/AI/EnemyScript.cs
+++ Unity Base Project/Assets/Scripts/AI/EnemyScript.cs
@@ -10,34 +10,69 @@ public class EnemyScript : MonoBehaviour
     public GameObject m_Player;
     public PlayerData m_playerInput;
     public GameObject explosion;
+    public GameObject[] Missiles;
+    public GameObject Missile;
+    public GameObject TargetPos;
+
 
     private float playerDistance;
+    private float missileCooldown;
+    private int missileCount;
+    private int missileCountLimit;
     private Vector3 playerDir;
     // Use this for initialization
     void Start()
     {
         playerDistance = 0.0f;
+        missileCooldown = 10.0f;
+        missileCount = 0;
+        missileCountLimit = 2;
         playerDir = m_Player.transform.position - transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerDistance = Vector3.Distance(m_Player.transform.position, transform.position);
-        Debug.Log("Distance to player is " + playerDistance);
+        if (missileCooldown > 0.0f)
+            missileCooldown -= Time.deltaTime;
 
-        if (playerDistance <= 200.0f)
+        playerDistance = Vector3.Distance(m_Player.transform.position, transform.position);
+        //Debug.Log("Distance to player is " + playerDistance);
+
+        if (playerDistance <= 200.0f && !m_playerInput.GetCloaked())
         {
             // turn towards player
             playerDir = m_Player.transform.position - transform.position;
-            Vector3 newEnemyDir = Vector3.RotateTowards(transform.forward, playerDir, Time.deltaTime / 5.0f, 0.0f);
+            Vector3 newEnemyDir = Vector3.RotateTowards(transform.forward, playerDir, Time.deltaTime / 10.0f, 0.0f);
             transform.rotation = Quaternion.LookRotation(newEnemyDir);
+
+            float angle = Vector3.Angle(newEnemyDir, playerDir);
+            if (angle <= 15.0f)
+                Fire();
         }
     }
 
     void Fire()
     {
+        if (missileCooldown <= 0.0f)
+        {
+            Missiles = GameObject.FindGameObjectsWithTag("Missile");
 
+            if (missileCount <= missileCountLimit - 1)
+            {
+                missileCooldown = 10.0f;
+                if (Missile != null)
+                {
+                    missileCount++;
+                    Instantiate(Missile, new Vector3(transform.localPosition.x, transform.localPosition.y - 10.0f, transform.localPosition.z + 10.0f), transform.rotation);
+                    Missile.GetComponent<EnemyMissile>().startTracking = true;
+                    Debug.Log("Begin Missile Tracking");
+
+                }
+                else
+                    Debug.Log("No Missile Gameobj attached");
+            }
+        }
     }
 
     void Kill()
