c311857fa06aff8b7a5f65a1759e4df91e7f1feb
Allan Murillo <noelmurillo15@fullsail.edu>
2016-04-20 10:28:26 -0400

    Mario crew members, enemy turns towards player when noticed =)

--- Unity Base Project/Assets/Scripts/AI/EnemyScript.cs
+++ Unity Base Project/Assets/Scripts/AI/EnemyScript.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class EnemyScript : MonoBehaviour {
+public class EnemyScript : MonoBehaviour
+{
 
     //  Player Info
     public float zOffset;
@@ -10,14 +11,31 @@ public class EnemyScript : MonoBehaviour {
     public PlayerData m_playerInput;
     public GameObject explosion;
 
-
+    private float playerDistance;
+    private Vector3 playerDir;
     // Use this for initialization
-    void Start () {
-	
-	}
+    void Start()
+    {
+        playerDistance = 0.0f;
+        playerDir = m_Player.transform.position - transform.position;
+    }
 
     // Update is called once per frame
     void Update()
+    {
+        playerDistance = Vector3.Distance(m_Player.transform.position, transform.position);
+        Debug.Log("Distance to player is " + playerDistance);
+
+        if (playerDistance <= 200.0f)
+        {
+            // turn towards player
+            playerDir = m_Player.transform.position - transform.position;
+            Vector3 newEnemyDir = Vector3.RotateTowards(transform.forward, playerDir, Time.deltaTime / 5.0f, 0.0f);
+            transform.rotation = Quaternion.LookRotation(newEnemyDir);
+        }
+    }
+
+    void Fire()
     {
 
     }
