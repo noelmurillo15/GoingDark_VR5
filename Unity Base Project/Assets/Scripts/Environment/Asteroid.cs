ba70ea357a43b7338b4789646a87f79364f69343
Allan Murillo <noelmurillo15@fullsail.edu>
2016-04-20 11:07:34 -0400

    Asteroid gen works and doesn't glitch

--- Unity Base Project/Assets/Scripts/Environment/Asteroid.cs
+++ Unity Base Project/Assets/Scripts/Environment/Asteroid.cs
@@ -3,40 +3,33 @@ using System.Collections;
 
 public class Asteroid : MonoBehaviour {
 
-    public float aliveTimer;
     public Vector3 m_scale;
     public Vector3 m_velocity;
     public Vector3 m_position;
     public Vector3 m_rotation;
     public AsteroidGenerator m_generator;
-    public GameObject m_player;
 
     // Use this for initialization
     void Start() {
 
-        if(m_player == null)
-            m_player = GameObject.FindGameObjectWithTag("Player");
+        m_velocity.x = Random.Range(-1.0f, 1.0f);
+        m_velocity.y = Random.Range(-1.0f, 1.0f);
+        m_velocity.z = Random.Range(-1.0f, 1.0f);
 
-        m_velocity.x = Random.Range(-15.0f, 15.0f);
-        m_velocity.y = Random.Range(-15.0f, 15.0f);
-        m_velocity.z = Random.Range(-8.0f, 8.0f);
-
-        m_position.x = Random.Range((m_player.transform.localPosition.x - 10.0f) - 200.0f, (m_player.transform.localPosition.x + 10.0f) + 200.0f);
-        m_position.y = Random.Range((m_player.transform.localPosition.y - 10.0f) - 200.0f, (m_player.transform.localPosition.y + 10.0f) + 200.0f);
-        m_position.z = Random.Range((m_player.transform.localPosition.z - 10.0f) - 50.0f,  (m_player.transform.localPosition.z + 10.0f) + 50.0f);
+        m_position.x = Random.Range(-10.0f, 10.0f);
+        m_position.y = Random.Range(-10.0f, 10.0f);
+        m_position.z = Random.Range(-10.0f, 10.0f);
 
         m_scale.x = Random.Range(50.0f, 200.0f);
         m_scale.y = Random.Range(50.0f, 200.0f);
         m_scale.z = Random.Range(50.0f, 200.0f);
 
-        m_rotation.x = Random.Range(-5.0f, 5.0f);
-        m_rotation.y = Random.Range(-5.0f, 5.0f);
-        m_rotation.z = Random.Range(-5.0f, 5.0f);
-
-        aliveTimer = Random.Range(30.0f, 120.0f);
+        m_rotation.x = Random.Range(-10.0f, 10.0f);
+        m_rotation.y = Random.Range(-10.0f, 10.0f);
+        m_rotation.z = Random.Range(-10.0f, 10.0f);
 
         if (m_generator == null)
-            m_generator = GameObject.Find("AsteroidGen").GetComponent<AsteroidGenerator>();
+            m_generator = GameObject.Find("Environment").GetComponent<AsteroidGenerator>();
 
         transform.localPosition = m_position;
         transform.localScale += m_scale;
@@ -44,9 +37,6 @@ public class Asteroid : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (aliveTimer > 0)
-            aliveTimer -= Time.deltaTime;
-        else DestroyAsteroid();
 
         if (CheckOutOfBounds())
             DestroyAsteroid();
@@ -56,11 +46,11 @@ public class Asteroid : MonoBehaviour {
     }
 
     bool CheckOutOfBounds() {
-        if (transform.localPosition.x > (m_player.transform.localPosition.x + 250.0f) || transform.localPosition.x < (m_player.transform.localPosition.x - 250.0f))
+        if (transform.localPosition.x > 500.0f || transform.localPosition.x < -500.0f)
             return true;                                                       
-        if (transform.localPosition.y > (m_player.transform.localPosition.y + 250.0f) || transform.localPosition.y < (m_player.transform.localPosition.x - 250.0f))
+        if (transform.localPosition.y > 500.0f || transform.localPosition.y < -500.0f)
             return true;
-        if (transform.localPosition.z > (m_player.transform.localPosition.x + 150.0f) || transform.localPosition.z < (m_player.transform.localPosition.x - 150.0f))
+        if (transform.localPosition.z > 500.0f || transform.localPosition.z < -500.0f)
             return true;
 
         return false;
