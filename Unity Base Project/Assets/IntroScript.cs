555b7aa8610fde1ce71743c659fb53acdf6f8587
Allan Murillo <noelmurillo15@fullsail.edu>
2016-04-25 10:12:51 -0400

    Intro Screen added

---
+++ Unity Base Project/Assets/IntroScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class IntroScript : MonoBehaviour {
+
+
+    [SerializeField]
+    Text mText;
+    [SerializeField]
+    Text skipText;
+    [SerializeField]
+    Image hands;
+
+    private float timer;
+    private bool skipActivate;
+	// Use this for initialization
+	void Start () {
+        timer = 5.0f;
+        skipActivate = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timer -= Time.deltaTime;
+        if (timer <= 0.0f)
+            skipActivate = true;
+
+        if (skipActivate && !skipText.IsActive())
+        {
+            skipText.gameObject.SetActive(true);
+            hands.gameObject.SetActive(true);
+
+            //if (!skipText.IsActive())
+            //{
+            //   
+            //}
+
+            //if (skipText.material.color.a == 1.0f)
+            //    skipText.CrossFadeAlpha(0.2f, 1.0f, false);
+            //else if (skipText.material.color.a == 0.2f)
+            //    skipText.CrossFadeAlpha(1.0f, 1.0f, false); 
+            //FadeOut();
+        }
+        mText.transform.Translate(Vector3.up * Time.deltaTime * 25);
+    }
+
+    IEnumerator FadeIn()
+    {
+       skipText.CrossFadeAlpha(1.0f, 0.5f, false);
+       yield return new WaitForSeconds(2.0f);
+       FadeOut();
+    }
+
+    IEnumerator FadeOut()
+    {
+        skipText.CrossFadeAlpha(0.2f, 0.5f, false);
+        yield return new WaitForSeconds(2.0f);
+    }
+}
