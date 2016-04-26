dc4045b0c09f27830b96e62e7fe338ba5487d52f
Allan Murillo <noelmurillo15@fullsail.edu>
2016-04-26 17:13:29 -0400

    Rod's Commit

--- Unity Base Project/Assets/PillarLighting.cs
+++ Unity Base Project/Assets/PillarLighting.cs
@@ -6,26 +6,40 @@ public class PillarLighting : MonoBehaviour {
     public float timer;
     public float alphaTimer;
     private Color lightColor;
+    private GameObject player;
 
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
         timer = 20.0f;
 
         if (transform.parent.tag == "Loot")
             lightColor = Color.cyan;
         else
             lightColor = Color.yellow;
+
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update () {
-        if (timer > 0) {
+        if (timer > 0)
+        {
             alphaTimer = 5.0f;
             timer -= 5.0f * Time.deltaTime;
         }
         else
-            MaterialShift();
+        {            
+            if (Vector3.Distance(player.transform.position, transform.position) < 150.0f)
+            {
+                Debug.Log("Reset Alpha");
+                lightColor.a = 0.0f;
+                GetComponent<Renderer>().material.SetColor("_TintColor", lightColor);
+            }
+            else
+                MaterialShift();
+        }
     }
 
     void MaterialShift()
--- Unity Base Project/Assets/SpinScript.cs
+++ Unity Base Project/Assets/SpinScript.cs
@@ -3,18 +3,21 @@ using System.Collections;
 
 public class SpinScript : MonoBehaviour
 {
-    public float x = 0.0f;
-    public float y = 0.0f;
-    public float z = 0.0f;
+    public float x;
+    public float y;
+    public float z;
     // Use this for initialization
     void Start()
     {
-
+        x = Random.Range(0.0f, 360.0f);
+        y = Random.Range(0.0f, 360.0f);
+        z = Random.Range(0.0f, 360.0f);
+        gameObject.transform.localEulerAngles = new Vector3(x, y, z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.Rotate(new Vector3(x, y, z), 2.0f * Time.deltaTime);
+        //gameObject.transform.Rotate(new Vector3(x, y, z), 2.0f * Time.deltaTime);
     }
 }
