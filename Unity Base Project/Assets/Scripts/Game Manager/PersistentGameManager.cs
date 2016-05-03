52249f48e75cb414416cd9dd5588b521bc240e4d
Allan Murillo <noelmurillo15@fullsail.edu>
2016-05-03 14:27:03 -0400

    Shaq's Implementation : Missile Launch & Alarm Sound FX
    Cliff's Implementaion:
    Added Updated Persistent Game Manager

--- Unity Base Project/Assets/Scripts/Game Manager/PersistentGameManager.cs
+++ Unity Base Project/Assets/Scripts/Game Manager/PersistentGameManager.cs
@@ -22,4 +22,28 @@ public class PersistentGameManager : MonoBehaviour {
 	void Update () {
 
 	}
+    #region Getters
+
+    int GetPlayerResources() { return PlayerPrefs.GetInt("Resources", 0); }
+    int GetPlayerCredits() { return PlayerPrefs.GetInt("Credits", 0); }
+    string GetPlayerName() { return PlayerPrefs.GetString("PlayerName", "Captain Planet"); }
+    int GetPlayerMissileCount() { return PlayerPrefs.GetInt("MissleCount", 10); }
+    int GetPlayerHealth() { return PlayerPrefs.GetInt("PlayerHealth", 3); }
+    int GetCurrentLevel() { return PlayerPrefs.GetInt("CurrentLevel", 0); }
+
+
+
+    #endregion
+
+    #region Setters
+    void SetPlayerResources(int ResourceCount) { PlayerPrefs.SetInt("Resources", ResourceCount); }
+    void SetPlayerCredits(int CreditCount) { PlayerPrefs.SetInt("Credits", CreditCount); }
+    void SetPlayerName(string PlayerName) { PlayerPrefs.SetString("PlayerName", PlayerName); }
+    void SetPlayerMissileCount(int MissileCount) { PlayerPrefs.SetInt("MissleCount", MissileCount); }
+    void SetPlayerHealth(int Health) { PlayerPrefs.SetInt("PlayerHealth", Health); }
+    void SetCurrentLevel(int Level) { PlayerPrefs.SetInt("CurrentLevel", Level); }
+
+
+    #endregion
+
 }
