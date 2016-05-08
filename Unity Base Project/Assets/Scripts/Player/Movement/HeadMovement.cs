b1d4d881ad74423a70b9b7ac08035983f719c171
Allan Murillo <noelmurillo15@fullsail.edu>
2016-05-08 11:43:01 -0400

    Allan's Implementation: Head movement does not depend on Player Move anymore

--- Unity Base Project/Assets/Scripts/Player/Movement/HeadMovement.cs
+++ Unity Base Project/Assets/Scripts/Player/Movement/HeadMovement.cs
@@ -3,18 +3,18 @@
 public class HeadMovement : MonoBehaviour {
     //**    Attach to Main Camera   **//
     private LeapData m_leapData;
-    private PlayerMovement m_playerMove;
+    private GameObject m_Player;
 
 
     // Use this for initialization
     void Start() {
         m_leapData = GameObject.FindGameObjectWithTag("LeapControl").GetComponent<LeapData>();
-        m_playerMove = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+        m_Player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update() {
         if(m_leapData.GetNumHands() == 0)
-            m_playerMove.SetPlayerRotation(this.transform.rotation);
+            m_Player.transform.rotation = Quaternion.Slerp(m_Player.transform.rotation, transform.rotation, Time.deltaTime * 0.5f);
     }
 }
