021b5b37fe388d522b06e1327695ebec0ffebaa8
Allan Murillo <noelmurillo15@fullsail.edu>
2016-05-11 14:51:51 -0400

    Joe's Implmentation : Portal Works

---
+++ Unity Base Project/Assets/Scripts/Environment/Portal.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour {
+    private float jumpRangeX, jumpRangeY, jumpRangeZ;
+    public Portal otherPortal;
+    private float jumpTimer;
+    public AudioSource teleSound;
+    //private RenderTexture RTT;
+    //private Texture texture;
+
+    // Use this for initialization
+    void Start () {
+        //GetComponent<Material>().mainTexture = GetComponentInChildren<PortalCam>().RTT;
+        //RTT = GetComponentInChildren<PortalCam>().RTT;
+        //texture = GetComponent<Renderer>().material.mainTexture;
+
+        jumpTimer = Random.Range(60, 90);
+
+        BoxCollider box = GameObject.Find("Environment").GetComponent<BoxCollider>();
+
+        jumpRangeX = box.size.x / 2;
+        jumpRangeY = box.size.y / 2;
+        jumpRangeZ = box.size.z / 2;
+
+        teleSound = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //GetComponent<Renderer>().material.mainTexture = GetComponentInChildren<PortalCam>().RTT;
+
+        jumpTimer -= Time.deltaTime;
+        if (jumpTimer <= 0.0f || Vector3.Distance(transform.position, otherPortal.transform.position) < 2400)
+        {
+            jumpTimer = Random.Range(60f, 90f);
+            transform.position = new Vector3(Random.Range(-jumpRangeX, jumpRangeX), Random.Range(-jumpRangeY, jumpRangeY), Random.Range(-jumpRangeZ, jumpRangeZ));
+        }
+    }
+
+    public void OnTriggerEnter(Collider col)
+    {
+        if (col.transform.tag == "Player")
+        {
+            col.transform.position = otherPortal.transform.position + otherPortal.transform.forward * 20;
+            teleSound.Play();
+            
+        }
+    }
+
+}
