21be79e96666c64b85e3871268dc012bedfa3e7d
Allan Murillo <noelmurillo15@fullsail.edu>
2016-06-01 13:52:59 -0400

    Jeff - Added Missile Switching using controller and displaying Missile Type to user.

--- Unity Base Project/Assets/Scripts/Core/ShipSystems/MissileSystem.cs
+++ Unity Base Project/Assets/Scripts/Core/ShipSystems/MissileSystem.cs
@@ -16,24 +16,26 @@ public class MissileSystem : ShipDevice
     private GameObject selectedMissile;
 
     private Transform MyTransform;
-    private Transform leapcam; 
+    private Transform leapcam;
 
     private bool missSwitch;
     private float missTimer;
-
+    private int Wchoice;
     //  Missile Display
     private Text textCount;
+    private Text textMissileChoice;
+
     #endregion
 
 
     void Start()
     {
         missSwitch = false;
-        missTimer = 5;
+        missTimer = 0.5f;
         Count = 15;
         maxCooldown = 1f;
+        Wchoice = 0;
 
-        
         basic = Resources.Load<GameObject>("Missiles/BasicMissile");
         emp = Resources.Load<GameObject>("Missiles/EmpMissile");
         chromatic = Resources.Load<GameObject>("Missiles/ChromaticMissile");
@@ -43,24 +45,68 @@ public class MissileSystem : ShipDevice
         leapcam = GameObject.FindGameObjectWithTag("MainCamera").transform;
 
         textCount = GameObject.Find("MissileCounter").GetComponent<Text>();
+        textMissileChoice = GameObject.Find("MissileChoice").GetComponent<Text>();
+
+        textMissileChoice.text = "Basic Missile";
         textCount.text = Count.ToString();
-        selectedMissile = chromatic;
+
+        selectedMissile = basic;
     }
 
-    void FixedUpdate()
+    void Update()
     {
         if (Input.GetKey(KeyCode.F))
             Activate();
 
-        if (Input.GetAxisRaw("LBumper") > 0f)
-            Activate();
         if (Input.GetAxisRaw("RBumper") > 0f)
             Activate();
+
+        if (Input.GetButtonDown("Y"))
+            missSwitch = true;
+
+        if (missSwitch /*&& missTimer <= 0*/ )
+        {
+            Wchoice++;
+            SwitchWeapon();
+            missSwitch = false;
+        }
 
         if (Activated)
             LaunchMissile();
 
         MyTransform.rotation = leapcam.rotation;
+    }
+
+    public void SwitchWeapon()
+    {
+        if (Wchoice >= 5)
+            Wchoice = 0;
+
+        if (Wchoice == 0)
+        {
+            selectedMissile = basic;
+            textMissileChoice.text = "Basic Selected"; //selectedMissile.ToString();
+            Debug.Log("basic is current weapon");
+        }
+        else if (Wchoice == 1)
+        {
+            selectedMissile = emp;
+            textMissileChoice.text = "EMP Selected";
+            Debug.Log("emp is current weapon");
+        }
+        else if (Wchoice == 2)
+        {
+            selectedMissile = shieldbreak;
+            textMissileChoice.text = "ShieldBreaker Selected"; //selectedMissile.ToString();
+            Debug.Log("shieldbreak is current weapon");
+        }
+        else if (Wchoice == 3)
+        {
+            selectedMissile = chromatic;
+            textMissileChoice.text = "Chromatic Selected";// selectedMissile.ToString();
+            Debug.Log("chromatic is current weapon");
+        }
+
     }
 
     public void LaunchMissile()
@@ -86,24 +132,28 @@ public class MissileSystem : ShipDevice
         int rand = Random.Range(3, 6);
         Count += rand;
         textCount.text = Count.ToString();
-    }  
-        
+    }
+
     public void WeaponSelect(MissileType type)
     {
         switch (type)
         {
             case MissileType.EMP:
                 selectedMissile = emp;
+                textMissileChoice.text = "EMP Selected";// selectedMissile.ToString();
                 break;
             case MissileType.BASIC:
                 selectedMissile = basic;
+                textMissileChoice.text = "Basic Selected"; //selectedMissile.ToString();
                 break;
             case MissileType.CHROMATIC:
                 selectedMissile = chromatic;
+                textMissileChoice.text = "Chromatic Selected";// selectedMissile.ToString();
                 break;
             case MissileType.SHIELDBREAKER:
                 selectedMissile = shieldbreak;
+                textMissileChoice.text = "ShieldBreaker";// selectedMissile.ToString();
                 break;
         }
-    }  
+    }
 }
