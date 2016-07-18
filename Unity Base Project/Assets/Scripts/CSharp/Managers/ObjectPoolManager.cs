a2be4ae44de456ca21e647a3dd61d25cf47ae7bd
Allan Murillo <noelmurillo15@fullsail.edu>
2016-07-18 17:40:22 -0400

    Allan
    Minor change

--- Unity Base Project/Assets/Scripts/CSharp/Managers/ObjectPoolManager.cs
+++ Unity Base Project/Assets/Scripts/CSharp/Managers/ObjectPoolManager.cs
@@ -2,6 +2,8 @@
 
 public class ObjectPoolManager : MonoBehaviour {
 
+    private Transform MyTransform;
+
     private ObjectPooling emp = new ObjectPooling();
     private ObjectPooling basic = new ObjectPooling();
     private ObjectPooling chromatic = new ObjectPooling();
@@ -19,20 +21,25 @@ public class ObjectPoolManager : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        ammopool.Initialize(Resources.Load<GameObject>("AmmoDrop"), 10, transform);
-        laserpool.Initialize(Resources.Load<GameObject>("Projectiles/Lasers/EnemyLaser"), 30, transform);
-        missilepool.Initialize(Resources.Load<GameObject>("Projectiles/Missiles/EnemyMissile"), 30, transform);
-        explosionpool.Initialize(Resources.Load<GameObject>("Projectiles/Explosions/EnemyExplosion"), 40, transform);
+        MyTransform = transform;
 
-        emp.Initialize(Resources.Load<GameObject>("Projectiles/Missiles/EmpMissile"), 4, transform);
-        basic.Initialize(Resources.Load<GameObject>("Projectiles/Missiles/BasicMissile"), 4, transform);
-        chromatic.Initialize(Resources.Load<GameObject>("Projectiles/Missiles/ChromaticMissile"), 4, transform);
-        shieldbreak.Initialize(Resources.Load<GameObject>("Projectiles/Missiles/ShieldBreakMissile"), 4, transform);
+        //  Enemy
+        ammopool.Initialize(Resources.Load<GameObject>("AmmoDrop"), 10, MyTransform);
+        laserpool.Initialize(Resources.Load<GameObject>("Projectiles/Lasers/EnemyLaser"), 30, MyTransform);
+        missilepool.Initialize(Resources.Load<GameObject>("Projectiles/Missiles/EnemyMissile"), 30, MyTransform);
+        explosionpool.Initialize(Resources.Load<GameObject>("Projectiles/Explosions/EnemyExplosion"), 40, MyTransform);
+        
+        //  Missiles
+        emp.Initialize(Resources.Load<GameObject>("Projectiles/Missiles/EmpMissile"), 4, MyTransform);
+        basic.Initialize(Resources.Load<GameObject>("Projectiles/Missiles/BasicMissile"), 4, MyTransform);
+        chromatic.Initialize(Resources.Load<GameObject>("Projectiles/Missiles/ChromaticMissile"), 4, MyTransform);
+        shieldbreak.Initialize(Resources.Load<GameObject>("Projectiles/Missiles/ShieldBreakMissile"), 4, MyTransform);
 
-        BasicLasers.Initialize(Resources.Load<GameObject>("Projectiles/Lasers/LaserBeam"), 12, transform);
-        ChargedLasers.Initialize(Resources.Load<GameObject>("Projectiles/Lasers/ChargedShot"), 12, transform);
-        BasicExplosion.Initialize(Resources.Load<GameObject>("Projectiles/Explosions/BasicExplosion"), 6, transform);
-        ChargedExplosion.Initialize(Resources.Load<GameObject>("Projectiles/Explosions/ChargeLaserExplosion"), 6, transform);
+        //  Lasers
+        BasicLasers.Initialize(Resources.Load<GameObject>("Projectiles/Lasers/LaserBeam"), 12, MyTransform);
+        ChargedLasers.Initialize(Resources.Load<GameObject>("Projectiles/Lasers/ChargedShot"), 12, MyTransform);
+        BasicExplosion.Initialize(Resources.Load<GameObject>("Projectiles/Explosions/BasicExplosion"), 6, MyTransform);
+        ChargedExplosion.Initialize(Resources.Load<GameObject>("Projectiles/Explosions/ChargeLaserExplosion"), 6, MyTransform);
     }
 
     #region Accessors
