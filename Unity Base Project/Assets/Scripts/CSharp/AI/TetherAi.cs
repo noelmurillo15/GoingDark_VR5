5e045ce73e85de844d0d7ad859847875f04e9a38
Allan Murillo <noelmurillo15@fullsail.edu>
2016-08-05 10:04:42 -0400

    Allan : Updated Enemy Prefabs Shaq : Added Tether AI to Trident enemy

--- Unity Base Project/Assets/Scripts/CSharp/AI/TetherAi.cs
+++ Unity Base Project/Assets/Scripts/CSharp/AI/TetherAi.cs
@@ -3,20 +3,41 @@
 public class TetherAi : MonoBehaviour
 {
     #region Properties
-    //  Enemy Data
+    ParticleSystem particles;
+    public float speed = 10;
+    public float emission = 20;
+
     private EnemyStateManager behavior;
     #endregion
 
-
-    // Use this for initialization
     void Start()
     {
+        particles = GetComponent<ParticleSystem>();
         behavior = GetComponent<EnemyStateManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-       
+
+        if (behavior.State == GoingDark.Core.Enums.EnemyStates.Attack)
+        {
+            particles.emissionRate = emission;
+            particles.startSpeed = speed;
+            ParticleSystem.Particle[] p = new ParticleSystem.Particle[particles.particleCount + 1];
+            Vector3 dir = behavior.Target.position - transform.position;
+            float dis = Vector3.Distance(behavior.Target.position, transform.position);
+            int l = particles.GetParticles(p);
+            for (int i = 0; i < l; i++)
+            {
+                p[i].velocity = dir;
+                p[i].velocity *= (speed * dis * Time.fixedDeltaTime);
+                Debug.Log("Tether Firing");
+            }
+
+            particles.SetParticles(p, l);
+        }
+
     }
+
+    
 }
