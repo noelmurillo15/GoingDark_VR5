b4d50e3c3f0d05a7b6a2f06f8fcd1b495ae609f2
Allan Murillo <noelmurillo15@fullsail.edu>
2016-08-10 13:39:30 -0400

    Joe: Credit scene

---
+++ Unity Base Project/Assets/Scripts/CSharp/Menu/CreditExit.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditExit : MonoBehaviour {
+
+    x360Controller m_Controller;
+    float width, height;
+    // Use this for initialization
+    void Start () {
+        m_Controller = GamePadManager.Instance.GetController(0);
+
+        width = Screen.width;
+        height = Screen.height;
+
+        Vector3 temp = new Vector3(width - 100, 0 + 50, 0);
+        transform.position = temp;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (m_Controller.GetButtonDown("B"))
+        {
+            LoadingScreenManager.LoadScene("MainMenu");
+        }
+        if (width != Screen.width || Screen.height != height)
+        {
+            width = Screen.width;
+            height = Screen.height;
+            Vector3 temp = new Vector3(width - 100, 0 + 50, 0);
+            transform.position = temp;
+        }
+    }
+}
