8071ad551a66753c966bba9b7f84e3099625852d
Allan Murillo <noelmurillo15@fullsail.edu>
2016-08-23 09:55:32 -0400

    Joe
    Level slect, credit update

---
+++ Unity Base Project/Assets/Scripts/CSharp/Menu/SpinText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinText : MonoBehaviour {
+
+    // Use this for initialization
+    Transform myTransform;
+    [SerializeField]
+    Transform centerPos;
+    [SerializeField]
+    float rotateSpeed;
+    void Start () {
+        myTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myTransform.RotateAround(centerPos.position,Vector3.down, rotateSpeed* Time.deltaTime);
+	}
+}
