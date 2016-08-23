bbb1a80f7917dfd7f4ade0fa53b00e9840aada05
Allan Murillo <noelmurillo15@fullsail.edu>
2016-08-23 10:13:29 -0400

    Allan
    Fixed Player Meshes in each level

---
+++ Unity Base Project/Assets/Scripts/CSharp/Player/AsteroidLoot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class AsteroidLoot : MonoBehaviour {
+
+    private MissionSystem mission;
+    private PlayerStats playerStats;
+
+    // Use this for initialization
+    void Start()
+    {
+        mission = GameObject.FindGameObjectWithTag("GameManager").GetComponent<MissionSystem>();
+        playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.transform.CompareTag("Player"))
+        {
+            mission.AsteroidLoot();
+            playerStats.UpdateCredits(100);
+            AudioManager.instance.PlayCollect();
+            Destroy(gameObject);
+        }
+    }
+}
